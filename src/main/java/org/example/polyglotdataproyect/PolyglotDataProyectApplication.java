5895e116e2f7defbdb4eb76a7b449c5ebb317583
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2025-10-29 10:20:47 -0500

    First commit. Added the general project structure.

---
+++ src/main/java/org/example/polyglotdataproyect/PolyglotDataProyectApplication.java
@@ -0,0 +1,13 @@
+package org.example.polyglotdataproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyglotDataProyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyglotDataProyectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/polyglotdataproyect/PolyglotDataProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.polyglotdataproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyglotDataProyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
